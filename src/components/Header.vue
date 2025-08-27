916839055266d41964cb62d74d8566837e693f37
CePseudo <hoffmann.axel.6032@gmail.com>
2025-08-27 12:37:40 +0200

    added fari logo in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,26 @@
 <script setup lang="ts">
 
 import {Button} from "@/components/ui/button";
+import FariLogo from "@/assets/FariLogo.vue";
 </script>
 
 <template>
-  <div class="flex flex-row w-full items-center justify-between py-4 px-25">
+  <div class="flex flex-row w-full items-center justify-between py-4 px-25 text-white bg-[#2250c6]">
     <router-link to="/">
-    <p class="font-semibold text-2xl font-sans">FARI Digital Twin Brussels</p>
+      <FariLogo/>
 
     </router-link>
     <div class="flex gap-2">
-      <Button variant="link" as-child>
+      <Button variant="ghost" as-child class="text-white hover:bg-white/10">
         <RouterLink to="/doc">
           API doc
         </RouterLink>
       </Button>
-      <Button variant="default">Sign in</Button>
+      <Button variant="secondary" as-child class="bg-white text-blue-800 hover:bg-gray-100">
+        <RouterLink to="/signin">
+          Sign in
+        </RouterLink>
+      </Button>
     </div>
   </div>
 </template>
